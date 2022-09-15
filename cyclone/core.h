44e863c0bb9f25b55e30ed34d001e445bd6e6bd4
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2022-09-15 10:55:53 -0400

    Update core.h and Vector3 class

--- cyclone/core.h
+++ cyclone/core.h
@@ -12,25 +12,110 @@ namespace cyclone {
 			real pad;
 
 		public:
+			/******* DEFAULT CONSTUCTOR *******/
 			Vector3() : x(0), y(0), z(0) {}
+			/******* EXPLICIT CONSTUCTOR *******/
 			Vector3(const real x, const real y, const real z) : x(x), y(y), z(z) {}
+
+			/******* FLIP ALL COMPONENTS OF THE VECTOR *******/
 			void invert() {
 				x = -x;
 				y = -y;
 				z = -z;
 			}
+			/******* VECTOR AND DIRECTION *******/
 			real magnitude() const {
-				return sqrt(x * x + y * y + z * z);
+				return (real)sqrt(x * x + y * y + z * z);
 			}
-			real squareMagnitud() const {
+			real squareMagnitude() const {
 				return x * x + y * y + z * z;
 			}
-			void print() {
-				std::cout << "Vector3 : x=" << x << "\t\ty=" << y << "\t\tz=" << z << std::endl;
+			void normalize() {
+				real l = magnitude(); 
+				if (l > 0) {					
+					(*this) *= ((real)1) / l;
+				} 
 			}
-			/*friend ostream& operator<<(ostream& os, const Vector3& vector) {
-				os << "Vector3 : x=" << vector.x << "\t\ty=" << vector.y << "\t\tz=" << vector.z;
-				return os;
+			/******* SCALAR AND VECTOR MULTIPLICATION *******/
+			void operator*=(const real value) {
+				x *= value; 
+				y *= value; 
+				z *= value;
+			}
+			Vector3 operator*(const real value) const {
+				return Vector3(x * value, y * value, z * value);
+			}
+			/******* VECTOR ADDITION *******/
+			void operator+=(const Vector3& v) {
+				x += v.x;
+				y += v.y;
+				z += v.z;
+			}
+			Vector3 operator+(const Vector3& v) const {
+				return Vector3(x + v.x, y + v.y, z + v.z);
+			}
+			/******* VECTOR SUBTRACTION *******/
+			void operator-=(const Vector3& v) {
+				x -= v.x;
+				y -= v.y;
+				z -= v.z;
+			}
+			Vector3 operator-(const Vector3& v) const {
+				return Vector3(x - v.x, y - v.y, z - v.z);
+			}
+			/******* VECTOR ADDITION AND SCALING *******/
+			void addScaledVector(const Vector3& v, real scale) {
+				x += v.x * scale;
+				y += v.y * scale;
+				z += v.z * scale;
+			}
+			/******* COMPONENT PRODUCT *******/
+			void componentProductUpdate(const Vector3& v) {
+				x *= v.x;
+				y *= v.y;
+				z *= v.z;
+			}
+			Vector3 componentProduct(const Vector3& v) const {
+				return Vector3(x * v.x, y * v.y, z * v.z);
+			}
+			/******* SCALAR PRODUCT *******/
+			real scalarProduct(const Vector3& v) const {
+				return x * v.x + y * v.y + z * v.z;
+			}
+			real operator*(const Vector3& v) const {
+				return x * v.x + y * v.y + z * v.z;
+			}
+			/******* VECTOR PRODUCT *******/
+			Vector3 vectorProduct(const Vector3& v) const {
+				return Vector3(y*v.z - z*v.y,
+							   z*v.x - x*v.z,
+							   x*v.y - y*v.x);
+			}
+			void operator%=(const Vector3& v) {
+				(* this) = vectorProduct(v);
+			}
+			Vector3 operator%(const Vector3& v) const {
+				return Vector3(y*v.z - z*v.y,
+							   z*v.x - x*v.z,
+							   x*v.y - y*v.x);
+			}
+			/******* ORTHONORMAL BASIS *******/
+			void makeOrthonormalBasis(Vector3 *a, Vector3 *b, Vector3 *c){
+				a->normalize();
+				(*c) = (*a) % (*b);
+				if (c->squareMagnitude() == 0.0) {
+					return;
+				}
+				c->normalize();
+				(*b) = (*c) % (*a);
+			}
+			/******* PRINTING FUNCTION *******/
+			/*void print() {
+				std::cout << "Vector3 : x=" << x << "\t\ty=" << y << "\t\tz=" << z << std::endl;
 			}*/
+
+			friend std::ostream& operator<<(std::ostream& os, const Vector3& vector) {
+				return os << "Vector3 : x=" << vector.x << "\t\ty=" << vector.y << "\t\tz=" << vector.z << std::endl;
+			}
 	};	
 }
